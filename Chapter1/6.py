f47f22d85ff16a8ee495b4c7585d50b4cfaf4f97
lygztq <lygztq@sjtu.edu.cn>
2017-09-10 21:35:54 +0800

    9-10

---
+++ Chapter1/6.py
@@ -0,0 +1,5 @@
+import nltk
+from nltk.book import *
+
+text2.dispersion_plot(["Elinor", "Marianne", "Edward", "Willoughby"])
+# couple: Elinor and Edward
---
+++ Chapter1/7.py
@@ -0,0 +1,4 @@
+import nltk
+from nltk.book import *
+
+text5.collocations()
